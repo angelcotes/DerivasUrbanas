7c73d550e206347ff1e39b9f4a90cb7421a681a2
angelcotes <angelcotes@gmail.com>
2016-10-01 19:48:18 -0500

    recuperacion

--- angular/controller/signUp.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('vista')
-.controller('singUpCtrl', function ($scope, AuthService) {
-
-	$scope.sendata = function () {      
-	  AuthService.signUp($scope.user);
-	};
-});
