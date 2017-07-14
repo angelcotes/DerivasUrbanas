c52aa61e1a05f7e64b8d2e22a918a628eb412761
angel cotes españa <angelcotes@gmail.com>
2017-07-14 00:19:06 -0500

    cambios menores

--- angular/controller/group/viewDocCtrl.js
+++ angular/controller/group/viewDocCtrl.js
@@ -1,8 +1,8 @@
 angular.module('vista')
   .controller('viewDocCtrl', function ($http, $timeout, data, $uibModalInstance, $scope, $uibModal, ViewActiv, $location, $route, StorageService) {
-    var documentParts = data.url.split(".");
+    var documentParts = data.split(".");
     $scope.format = documentParts[documentParts.length - 1];
-    $scope.url = data.url;
+    $scope.url = data;
 
     $scope.cancelar = function(){
       $route.reload();
